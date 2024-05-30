3fd8de9541fc5d0922e66206a04583cfd05b1fbd
Eric Ferrari-Herrmann <eric.ferrari-herrmann@students.ffhs.ch>
2024-05-30 14:28:52 +0200

    Initial commit

---
+++ src/main/java/com/example/jplferrarijobhunt/JplFerrariJobhuntApplication.java
@@ -0,0 +1,13 @@
+package com.example.jplferrarijobhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JplFerrariJobhuntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JplFerrariJobhuntApplication.class, args);
+    }
+
+}
